65fbf76bc8d6e37e0622cf4d12d9e8f13fe4f9a8
Abanoub Essam <abanoub.e.mhanna@gmail.com>
2020-11-29 22:26:35 +0200

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [opened, setOpened] = useState(false)
+
+  const handleClickOpen = () => {
+    setOpened(!opened)
+  }
+  
+  const sidenavState = opened ? "openNavSidenav" : "closeNavSidenav";
+  const mainState = opened ? "openNavMain" : "closeNavMain";
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="mySidenav" className={`sidenav ${sidenavState}`}>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+      </div>
+
+      <div id="main" className={mainState}>
+        <h2>Sidenav Push Example</h2>
+        <p>Click on the element below to open the side navigation menu, and push this content to the right.</p>
+        <span style={{ fontSize: "30px", cursor: "pointer" }} onClick={handleClickOpen}>&#9776; open</span>
+      </div>
+
     </div>
   );
 }
